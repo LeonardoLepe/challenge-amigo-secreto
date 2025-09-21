0611dcdb8abebea9639e25b1ffacaf7211f6843c
LeonardoLepe <laraleonardo774@gmail.com>
2025-09-21 15:27:18 -0500

    validacion de entrada app.js

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ let amigos = [];
  function agregarAmigo() {
     const input = document.getElementById('amigo');
     const nombre = input.value.trim();
+
+if (nombre === '') {
+        alert('Por favor ingresa un nombre válido');
+        return;
+    }
